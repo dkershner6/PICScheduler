d024214c63912cb49080f22dfc0d55f875f82fe1
Derek Kershner <kershner@eformedpartners.com>
2018-11-18 16:42:03 -0800

    Initial Commit

---
+++ PICScheduler/Data/Data.cs
@@ -0,0 +1,16 @@
+﻿using PICScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PICScheduler.Data
+{
+    public class Data
+    {
+        public static List<Employee> Employees = new List<Employee>()
+        {
+            new Employee{ Id = 1, Name = "Sarah", Priority = 1, NurseLevel = NurseLevel.RN, NightPreference = false, SundayPreference = 7, SaturdayPreference = 6, FridayPreference = 5, WednesdayPreference = 4, ThursdayPreference = 3, TuesdayPreference = 2, MondayPreference = 1},
+            new Employee{ Id = 1, Name = "Dani", Priority = 2, NurseLevel = NurseLevel.RN, NightPreference = false, SundayPreference = 7, SaturdayPreference = 6, FridayPreference = 5, WednesdayPreference = 4, ThursdayPreference = 3, TuesdayPreference = 2, MondayPreference = 1},
+        };
+    }
+}
---
+++ PICScheduler/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PICScheduler.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public NurseLevel NurseLevel { get; set;}
+        public int? Priority { get; set; }
+        public bool? NightPreference { get; set; }
+        public int? SundayPreference { get; set; }
+        public int? MondayPreference { get; set; }
+        public int? TuesdayPreference { get; set; }
+        public int? WednesdayPreference { get; set; }
+        public int? ThursdayPreference { get; set; }
+        public int? FridayPreference { get; set; }
+        public int? SaturdayPreference { get; set; }
+    }
+
+    public enum NurseLevel
+    {
+        RN,
+        MA
+    }
+}
